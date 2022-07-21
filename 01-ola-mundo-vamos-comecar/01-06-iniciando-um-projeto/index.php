cdd3ea10611b19f4174b417d7df49b21eec15a0e
JoaoVitor-Dev <joaovitorreal@outlook.com>
2022-07-20 21:22:45 -0300

    Arquivos do Módulo

---
+++ 01-ola-mundo-vamos-comecar/01-06-iniciando-um-projeto/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="with=device=with, initial-scale=1">
+
+    <title>FSPHP: Iniciando Um Projeto</title>
+
+
+</head>
+
+<body>
+<?php
+/**
+ * Bloco PHP com variável start
+ * e comando de saída
+ */
+$start = "Vamos Começar";
+echo "<h1>Olá mundo! {$start}</h1>";
+echo "<div id='js'>Loading...</div>";
+?>
+
+<script src="./assets/script.js"></script>
+
+
+</body>
+
+</html>
