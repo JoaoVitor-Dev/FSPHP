8ff0bc0d4252cbc5d3ba492850ca1b177054c9dc
JoaoVitor-Dev <joaovitorreal@outlook.com>
2022-07-20 23:21:09 -0300

    Modulos do curso

--- 02-iniciando-do-zero-com-php/02-03-comandos-de-saida/index.php
+++ 02-iniciando-do-zero-com-php/02-03-comandos-de-saida/index.php
@@ -7,32 +7,77 @@ fullStackPHPClassName("02.03 - Comandos de saída");
  */
 fullStackPHPClassSession("echo", __LINE__);
 
+echo "<p>Olá Mundo!", " ", "<span class='tag'>#BoraProgramar!</span>", "</p>";
+
+$hello = "Olá mundo!";
+$code = "<span class='tag'>#BoraProgramar!</span>";
+
+echo "<p>$hello</p>";
+echo '<p>$hello</p>';
+
+$day = "dia";
+echo  "<p>Falta 1 $day para o evento</p>";
+echo  "<p>Falta 1 {$day}s para o evento</p>";
+
+echo '<h3>' . $hello . " " . $code . "</h3>";
+
+?>
+<h4><?= $hello; ?> <?= $code; ?></h4>
+
+<?php
 
 /**
  * [ print ] https://php.net/manual/pt_BR/function.print.php
  */
 fullStackPHPClassSession("print", __LINE__);
 
+//print $hello, $code;
+print $hello;
+print $code;
 
+print "<h3>{$hello} {$code}</h3>";
 /**
  * [ print_r ] https://php.net/manual/pt_BR/function.print-r.php
  */
 fullStackPHPClassSession("print_r", __LINE__);
+//print_r é um print utilizado em Vetores;
 
+$array = [
+  "company" => "Upinside",
+    "course" => "FSPHP",
+    "class" => "Comandos de saída"
+];
+
+//echo $array; Error
+//print $array; Error
+print_r($array);
+echo "<pre>", print_r($array, true), "</pre>";
 
 /**
  * [ printf ] https://php.net/manual/pt_BR/function.printf.php
  */
 fullStackPHPClassSession("printf", __LINE__);
 
+$article = "<article><h1>%s</h1><p>%s</p></article>";
+$title = "{$hello} {$code}";
+$subtitle = "Lorem";
+
+printf($article,$title, $subtitle);
 
 /**
  * [ vprintf ] https://php.net/manual/pt_BR/function.vprintf.php
  */
 fullStackPHPClassSession("vprintf", __LINE__);
 
-
+$company = "<article><h1>Escola %s</h1><p>curso <b>%s</b>, aula <b>%s</b></p></article>";
+vprintf($company, $array);
 /**
  * [ var_dump ] https://php.net/manual/pt_BR/function.var-dump.php
  */
 fullStackPHPClassSession("var_dump", __LINE__);
+
+var_dump(
+        $array,
+        $hello,
+        $code
+);
