a1d6923fffc0e8ba3f93d041c0a4f1a3bd2e57e1
JoaoVitor-Dev <joaovitorreal@outlook.com>
2022-07-25 15:28:19 -0300

    Trabalhando com Funções

---
+++ 02-iniciando-do-zero-com-php/02-11-trabalhando-com-funcoes/functions.php
@@ -0,0 +1,33 @@
+<?php
+
+function functionName($arg1, $arg2, $arg3){
+    $body = [$arg1, $arg2, $arg3];
+    return $body;
+}
+
+function optionArgs ($arg1, $arg2 = true, $arg3 = null){
+    //Os primeiros argumentos são obrigatórios,
+    //Os proximos argumentos são os importantes,
+    //Os ultimos são os opcionais;
+    $body = [$arg1, $arg2, $arg3];
+    return $body;
+}
+
+function calcImc(){
+    global $weight;
+    global $height;
+    return $weight / ($height * $height);
+}
+
+function payTotal($price){
+    static $total;
+    $total += $price;
+    return "<p>O total a pagar é R$".number_format($total,"2",",", ".")."</p>";
+}
+
+function myTeam()
+{
+    $teamNames = func_get_args();
+    $teamCount = func_num_args();
+    return [$teamNames, "count" => $teamCount];
+}
